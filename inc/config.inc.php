45214f386c2601033d0878020d04ddb88d748c4e
marcinlawnik <marcin.safmb@wp.pl>
2011-04-09 12:09:13 -0700

    Edited inc/config.inc.php via GitHub

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -18,9 +18,10 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+echo 'No hacking!';
 $CONF                      = array();
 
-//Jêzyk
+//Jï¿½zyk
 //Language
 $CONF['lang']              = 'pl';
 // $lang = 'pl';
@@ -29,13 +30,13 @@ $CONF['lang']              = 'pl';
 $CONF['quiz_name']         = 'example';
 // $quiz_name = 'example';
 //What will appear on start page
-//Co siê wyœwietli na stronie g³ównej
+//Co siï¿½ wyï¿½wietli na stronie gï¿½ï¿½wnej
 $CONF['start_content']     = '<p>Zasady s&#261; proste
 <br>1. Jest 15 pyta&#324;
 <br>2. Odpowied&#378; na ka&#380;de pytanie mo&#380;na znale&#378;&#263; w <a href="http://www.pl.wikipedia.org/">Wikipedii, wolnej encyklopedii</a> i <a href="http://pl.wikipedia.org/wiki/Szablon:Siostrzane">jej projektach siostrzanych</a> lub szukaj&#261;c w <a href="http://www.google.pl/">Google</a>.
 <br>3. Staraj si&#281; pisa&#263; poprawnie. Je&#380;eli jeste&#347; pewien odpowiedzi a skrypt podaje &#380;e jest b&#322;&#281;dna, spr&#243;buj napisa&#263; odpowied&#378; inaczej (z ogonkami, z du&#380;ej litery).</p>';
 
-//linki na stronie g³ównej (musz¹ byæ z http)
+//linki na stronie gï¿½ï¿½wnej (muszï¿½ byï¿½ z http)
 //links on start page (must be with http!!)
 
 //link1
@@ -47,7 +48,7 @@ $link2 = 'http://www.example.net';//page name
 $link2_name = 'example2';
 
 //what will appear on "you have won" page
-//co siê pojawi na stronie "wygra³eœ"
+//co siï¿½ pojawi na stronie "wygraï¿½eï¿½"
 $CONF['won_page_content']  = 'you have won';
 
 
@@ -57,6 +58,6 @@ $CONF['measure_time']  = 'TRUE';
 //-----------------
 // Don't change anything below this line unless you know what you are doing.
 //-----------------
-
+//commented out in quiz.php
 $CONF['debug_mode'] = 'FALSE';
 ?>
