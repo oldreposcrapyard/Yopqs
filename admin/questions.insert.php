7cdd4b8356f8aa7bf2860cf0eea50360f49be60f
Marcin Lawniczak <marcin.safmb@wp.pl>
2011-04-10 00:26:22 +0200

    Translated bbcode editor

--- admin/questions.insert.php
+++ admin/questions.insert.php
@@ -55,7 +55,150 @@ function dodaj_element(kontener){
 input.input { display: block; }
 -->
 </style>
-<script type="text/javascript" src="bbeditor/ed.js"></script>  
+<script type="text/javascript">
+/*****************************************/
+// Name: Javascript Textarea BBCode Markup Editor
+// Version: 1.3
+// Author: Balakrishnan
+// Last Modified Date: 25/jan/2009
+// License: Free
+// URL: http://www.corpocrat.com
+/******************************************/
+var textarea;$LANG[bold]
+var content;
+document.write("<link href=\"bbeditor/styles.css\" rel=\"stylesheet\" type=\"text/css\">");
+
+
+function edToolbar(obj) {
+    document.write("<div class=\"toolbar\">");
+	document.write("<img class=\"button\" src=\"bbeditor/images/bold.gif\" name=\"btnBold\" title=\"$LANG[bold]\" onClick=\"doAddTags('[b]','[/b]','" + obj + "')\">");
+    document.write("<img class=\"button\" src=\"bbeditor/images/italic.gif\" name=\"btnItalic\" title=\"$LANG[italic]\" onClick=\"doAddTags('[i]','[/i]','" + obj + "')\">");
+	document.write("<img class=\"button\" src=\"bbeditor/images/underline.gif\" name=\"btnUnderline\" title=\"$LANG[underline]\" onClick=\"doAddTags('[u]','[/u]','" + obj + "')\">");
+	document.write("<img class=\"button\" src=\"bbeditor/images/link.gif\" name=\"btnLink\" title=\"$LANG[inserturl]\" onClick=\"doURL('" + obj + "')\">");
+	document.write("<img class=\"button\" src=\"bbeditor/images/picture.gif\" name=\"btnPicture\" title=\"$LANG[insertimg]\" onClick=\"doImage('" + obj + "')\">");
+	document.write("<img class=\"button\" src=\"bbeditor/images/quote.gif\" name=\"btnQuote\" title=\"$LANG[insertquote]\" onClick=\"doAddTags('[quote]','[/quote]','" + obj + "')\">"); 
+  	document.write("<img class=\"button\" src=\"bbeditor/images/code.gif\" name=\"btnCode\" title=\"$LANG[insertcode]\" onClick=\"doAddTags('[code]','[/code]','" + obj + "')\">");
+    document.write("<img class=\"button\" src=\"bbeditor/images/youtube.png\" name=\"btnYoutube\" title=\"$LANG[insertyt]\" onClick=\"doAddTags('[youtube]','[/youtube]','" + obj + "')\">");
+	document.write("</div>");
+	//document.write("<textarea id=\""+ obj +"\" name = \"" + obj + "\" cols=\"" + width + "\" rows=\"" + height + "\"></textarea>");
+				}
+
+function doImage(obj)
+{
+textarea = document.getElementById(obj);
+var url = prompt('$LANG[enterimg]','http://');
+var scrollTop = textarea.scrollTop;
+var scrollLeft = textarea.scrollLeft;
+
+if (url != '' && url != null) {
+
+	if (document.selection) 
+			{
+				textarea.focus();
+				var sel = document.selection.createRange();
+				sel.text = '[img]' + url + '[/img]';
+			}
+   else 
+    {
+		var len = textarea.value.length;
+	    var start = textarea.selectionStart;
+		var end = textarea.selectionEnd;
+		
+        var sel = textarea.value.substring(start, end);
+	    //alert(sel);
+		var rep = '[img]' + url + '[/img]';
+        textarea.value =  textarea.value.substring(0,start) + rep + textarea.value.substring(end,len);
+		
+			
+		textarea.scrollTop = scrollTop;
+		textarea.scrollLeft = scrollLeft;
+	}
+}
+
+}
+
+function doURL(obj)
+{
+textarea = document.getElementById(obj);
+var url = prompt('$LANG[enterurl]','http://');
+var scrollTop = textarea.scrollTop;
+var scrollLeft = textarea.scrollLeft;
+
+if (url != '' && url != null) {
+
+	if (document.selection) 
+			{
+				textarea.focus();
+				var sel = document.selection.createRange();
+				
+			if(sel.text==""){
+					sel.text = '[url]'  + url + '[/url]';
+					} else {
+					sel.text = '[url=' + url + ']' + sel.text + '[/url]';
+					}			
+
+				//alert(sel.text);
+				
+			}
+   else 
+    {
+		var len = textarea.value.length;
+	    var start = textarea.selectionStart;
+		var end = textarea.selectionEnd;
+		
+        var sel = textarea.value.substring(start, end);
+		
+		if(sel==""){
+				var rep = '[url]' + url + '[/url]';
+				} else
+				{
+				var rep = '[url=' + url + ']' + sel + '[/url]';
+				}
+	    //alert(sel);
+		
+        textarea.value =  textarea.value.substring(0,start) + rep + textarea.value.substring(end,len);
+		
+			
+		textarea.scrollTop = scrollTop;
+		textarea.scrollLeft = scrollLeft;
+	}
+ }
+}
+
+function doAddTags(tag1,tag2,obj)
+{
+textarea = document.getElementById(obj);
+	// Code for IE
+		if (document.selection) 
+			{
+				textarea.focus();
+				var sel = document.selection.createRange();
+				//alert(sel.text);
+				sel.text = tag1 + sel.text + tag2;
+			}
+   else 
+    {  // Code for Mozilla Firefox
+		var len = textarea.value.length;
+	    var start = textarea.selectionStart;
+		var end = textarea.selectionEnd;
+		
+		
+		var scrollTop = textarea.scrollTop;
+		var scrollLeft = textarea.scrollLeft;
+
+		
+        var sel = textarea.value.substring(start, end);
+	    //alert(sel);
+		var rep = tag1 + sel + tag2;
+        textarea.value =  textarea.value.substring(0,start) + rep + textarea.value.substring(end,len);
+		
+		textarea.scrollTop = scrollTop;
+		textarea.scrollLeft = scrollLeft;
+		
+		
+	}
+}
+</script>  
 </head>
 <body>
 <form action="$PHP_SELF" method="post">
--- lang/pl.lang.php
+++ lang/pl.lang.php
@@ -15,7 +15,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 //************************
-//Polish - PL Language file for YOPQS 0.8 Beta on www.sourceforge.net
+//Polish - PL Language file for Yopqs 0.9 Beta on github.com
 //************************
 
 $LANG                      = array();
@@ -30,7 +30,15 @@ $LANG['startquiz']         = 'Zacznij Quiz!';
 $LANG['answerempty']       = 'Nie poda&#322;e&#347; odpowiedzi!';
 $LANG['of']                = 'z';
 $LANG['level']             = 'Poziom';
-
+$LANG['bold']              = 'Pogrubienie';
+$LANG['italic']            = 'Kursywa';
+$LANG['underline']         = 'Podkreślenie';
+$LANG['inserturl']         = 'Wstaw URL';
+$LANG['insertimg']         = 'Wstaw obrazek';
+$LANG['insertquote']       = 'Wstaw cytat';
+$LANG['insertyt']          = 'Wstaw filmik z YouTube';
+$LANG['enterurl']          = 'Podaj adres strony:';
+$LANG['enterimg']          = 'Podaj adres obrazka:';
 //Database
 $LANG['db_connect_error']  = 'Nie mo&#380;na po&#322;&#261;czy&#263; si&#281; z baz&#261; danych!';
 $LANG['db_select_error']   = 'Nie mo&#380;na wybra&#263; bazy danych!';
