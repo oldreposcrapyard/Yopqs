b99c1273d81e776f89311a0edc565b4ec335e5bd
Marcin Lawniczak <marcin.safmb@wp.pl>
2011-04-27 22:50:39 +0200

    Added parsing bbcode to start content in index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 require_once 'inc/config.inc.php';
 require_once "lang/{$CONF['lang']}.lang.php";
 require_once 'inc/bbcode/BbCode.class.php';
-require_once('inc/template_tbs.php');
+require_once 'inc/template_tbs.php';
+require_once 'inc/tbs_plugin_html.php';
 $TBS = new clsTinyButStrong;
 $quiz_name     = $CONF['quiz_name'];
 $main_page     = $LANG['mainpageuppercase'];
@@ -14,9 +15,9 @@ $link1_name    = $CONF['link1_name'];
 $link2         = $CONF['link2'];
 $link2_name    = $CONF['link2_name'];
 
-//$bb            = new BbCode();
-//$bb->parse($start_content, false);
-//$start_content = $bb->getHtml();
+$bb            = new BbCode();
+$bb->parse($start_content, false);
+$start_content = $bb->getHtml();
 
 $template      = $CONF['template'];
 $TBS->LoadTemplate("templates/$template/index.tpl");
