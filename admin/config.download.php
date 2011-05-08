a5afab10838537956d298c2ad9f2987871a91dad
Marcin Lawniczak <marcin.safmb@wp.pl>
2011-05-08 17:04:23 +0200

    Removed all files in admin folder because they were insecure and they didnt work

--- admin/config.download.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-type: text/plain");
-
-// It will be called downloaded.pdf
-header('Content-Disposition: attachment; filename="config.inc.php"');
-
-// The PDF source is in original.pdf
-readfile('inc/config.inc.php');
-?>
