bae7ae3d2c6710e7dbc0d039f2b4de59caf2e66e
Marcin Lawniczak <marcin.safmb@wp.pl>
2011-06-21 19:11:54 +0200

    [BBCODE] Updated parser

--- inc/bbcode/Filters/List.php
+++ inc/bbcode/Filters/List.php
@@ -7,32 +7,31 @@
  * @author wookieb
  * @version 1.1
  */
-class BbCodeFilterList
-{
-	public $tags=array(
-		'ul'=>array(
-			'open'=>'ul',
-			'close'=>'ul',
-			'wrap_white_space'=>true,
-			'allowed_child'=>array('li'),
-			'notallowed_parent'=>array('ol','ul'),
-			'parse_body'=>'parseList'
+class BbCodeFilterList {
+	public $tags = array(
+		'ul' => array(
+			'open' => 'ul',
+			'close' => 'ul',
+			'wrap_white_space' => true,
+			'allowed_child' => array('li'),
+			'notallowed_parent' => array('ol', 'ul'),
+			'parse_body' => 'parseList'
 		),
-		'li'=>array(
-			'open'=>'li',
-			'close'=>'li',
-			'allowed_parent'=>array('ul', 'ol')
+		'li' => array(
+			'open' => 'li',
+			'close' => 'li',
+			'allowed_parent' => array('ul', 'ol')
 		),
-		'ol'=>array(
-			'open'=>'ol',
-			'close'=>'ol',
-			'wrap_white_space'=>true,
-			'allowed_child'=>array('li'),
-			'notallowed_parent'=>array('ol','ul'),
-			'parse_body'=>'parseList'
+		'ol' => array(
+			'open' => 'ol',
+			'close' => 'ol',
+			'wrap_white_space' => true,
+			'allowed_child' => array('li'),
+			'notallowed_parent' => array('ol', 'ul'),
+			'parse_body' => 'parseList'
 		),
 	);
-	
+
 	/**
 	 * Parsuje LISTY
 	 * @param array $tag
@@ -41,22 +40,18 @@ class BbCodeFilterList
 	 * @param array $closeNode
 	 * @param BbCodeSettings $settings
 	 */
-	public function parseList($tag, &$openNode, &$body, &$closeNode, $settings)
-	{	
-		$good=false;
-		foreach($body as &$el)
-		{
-			if($el['type'] == BbCode::NODE_TYPE_OPEN && $el['tagname']=='li')
-			{
-				$good=true;
+	public function parseList($tag, &$openNode, &$body, &$closeNode, $settings) {
+		$good = false;
+		foreach ($body as &$el) {
+			if ($el['type'] == BbCode::NODE_TYPE_OPEN && $el['tagname'] == 'li') {
+				$good = true;
 				break;
 			}
 		}
-		
-		if(!$good)
-		{
-			$openNode=$settings->removeNode($openNode);
-			$closeNode=$settings->removeNode($closeNode);
+
+		if (!$good) {
+			$openNode = $settings->removeNode($openNode);
+			$closeNode = $settings->removeNode($closeNode);
 		}
 	}
 }
