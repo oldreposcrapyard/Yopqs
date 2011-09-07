82d1bfbb5a409e24731663e955500ef078a81774
Marcin Lawniczak <marcin.safmb@wp.pl>
2011-09-07 21:52:41 +0200

    Admin panel, try 3

--- admin/index.php
+++ admin/index.php
@@ -8,14 +8,11 @@ require '../inc/tbs_plugin_html.php';
 header('Content-Type: text/html; charset=utf-8');
 
 Alibaba::forceAuthentication();
-if(isset($_GET['module']) && file_exists('./'.$_GET['module'].'.php')
-{
-   include $_GET['module'] . '.php';
-}
-else
-{
-$TBS              = new clsTinyButStrong;
-$TBS->LoadTemplate("../templates/$CONF[template]/admin_index.tpl");
-$TBS->Show();
+if (isset($_GET['module']) && file_exists('./' . $_GET['module'] . '.php')) {
+    include $_GET['module'] . '.php';
+} else {
+    $TBS = new clsTinyButStrong;
+    $TBS->LoadTemplate("../templates/$CONF[template]/admin_index.tpl");
+    $TBS->Show();
 }
 ?>
