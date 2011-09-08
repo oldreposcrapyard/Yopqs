d2b2ba36a0e7307556acbbb579e96a38526e4642
Marcin Lawniczak <marcin.safmb@wp.pl>
2011-09-08 21:32:35 +0200

    Admin panel, try 19

--- admin/level.php
+++ admin/level.php
@@ -1,6 +1,5 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
-echo 'x1';
 require '../inc/config.inc.php';
 require '../inc/alibaba.class.php';
 require '../inc/allfunctions.inc.php';
@@ -13,31 +12,26 @@ try {
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
     ));
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'x';
 }
 catch (PDOException $e) {
     echo $LANG['db_connect_error'] . $e->getMessage();
     exit;
 }
-echo "<table border='1' cellpadding='10'>";
-echo "<tr> <th>ID</th> <th>First Name</th> <th></th> <th></th></tr>";
+
 try {
     $stmt = $pdo->query('SELECT * FROM Levels');
-    echo 'x';
-    while ($row = $stmt->fetch()) {
-        // echo out the contents of each row into a table
-        echo "<tr>";
-        echo '<td>' . $row['ID_lvl'] . '</td>';
-        echo '<td>' . $row['Question'] . '</td>';
-        echo '<td><a href="edit.php?id=' . $row['id'] . '">Edit</a></td>';
-        echo '<td><a href="delete.php?id=' . $row['id'] . '">Delete</a></td>';
-        echo "</tr>";
+$table_contents = '';    
+while ($row = $stmt->fetch()) {
+        $table_contents .= '<tr>';
+        $table_contents .= '<td>' . $row['ID_lvl'] . '</td>';
+        $table_contents .= '<td>' . $row['Question'] . '</td>';
+        $table_contents .= '<td><a href="edit.php?id=' . $row['ID_lvl'] . '">Edit</a></td>';
+        $table_contents .= '<td><a href="delete.php?id=' . $row['ID_lvl'] . '">Delete</a></td>';
+        $table_contents .= '</tr>';
     }
     $stmt->closeCursor();
 }
 catch (PDOException $e) {
     return $e->getMessage();
 }
-// close table
-echo "</table>";
 ?>
